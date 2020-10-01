e9e51b2e37e07e82bbc64a9270aacee64b9e37e7
Erik-Mtz <erik.martinez98@outlook.com>
2020-10-01 10:48:44 -0600

    made mongo variable

--- fric/app.js
+++ fric/app.js
@@ -1,10 +1,31 @@
 var express = require("express");
 var path = require("path");
+var bodyParser = require('body-parser');
+var mongo = require('mongoose');
 var expressValidator = require("express-validator");
 var expressSession = require("express-session");
 
+var db = mongo.connect("mongodb://localhost:27017/AnglarCRUD", function(err, response){
+    if(err){ console.log(err); }
+    else { console.log('Connected to',db,'+',response); }
+})
+
 /** Set up express variable */
 var app = express();
+app.use(bodyParser());
+app.use(bodyParser.json({limit:'5mb'}));
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost4200');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+});
+
+var Schema = mongo.Schema;
+
 
 // Set the port to 4000
 app.set("port", process.env.PORT || 4000);
