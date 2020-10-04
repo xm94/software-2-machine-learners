7d327b7cef945747f880ced943b89866e46a8cf0
Erik-Mtz <erik.martinez98@outlook.com>
2020-10-03 23:01:00 -0600

    able to call backend through front end, (login button)

--- server/server.js
+++ server/server.js
@@ -17,23 +17,39 @@ app.set("port", process.env.PORT || 4000);
 //var routes = require("./routes");
 /** Include routes.js file */
 // app.use(routes);
-app.get("/ping", async (req, res) => {
+
+app.get("/", async function(req,res,next){
+    console.log("Getting /");
     try {
-        console.log("Getting ping try");
         const items = await fric.find({});
         res.json(items);
     } catch (error){
+        next(error);
+    }
+})
+
+app.get("/ping", function(req, res, next){
+    try {
+        console.log("Getting ping try");
+        // const items = await fric.find({});
+        // res.json(items);
+        res.write("THIS IS THE BODY RETURNED app.get(ping) ............");
+        //res.send("ok");
+        return;
+    } catch (error){
         console.log("Getting ping error");
         console.log("Error");
+        console.log(error);
     }
-    res.send("ok");
-});
+})
 
 app.get("/login", (req, res) => {
+  console.log("Getting login");
   res.send("ok");
 });
 
 app.post("/events", (req, res) => {
+    console.log("Posting events");
     res.send("ok");
   });
 /** Start local host */
