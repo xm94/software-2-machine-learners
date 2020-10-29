2f30da3edd4d581d1945be741850d8de36726238
Erik-Mtz <erik.martinez98@outlook.com>
2020-10-29 12:10:57 -0600

    updating files to make merge possible

--- server/dao/tasks.js
+++
@@ -1,6 +0,0 @@
-var express = require("express");
-const { Sequelize, DataTypes, Model } = require('sequelize');
-const sequelize = new Sequelize('postgres://localhost:5432/fric_test') // Example for postgres
-var transactionLogs = require("./transactionLogs");
-
-
