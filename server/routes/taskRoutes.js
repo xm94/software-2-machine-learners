80faa9449bc0b5da8a92c23cd2a8fd6428318a0b
Erik-Mtz <erik.martinez98@outlook.com>
2020-10-29 13:39:00 -0600

    added ability to add task

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -21,7 +21,7 @@ router.get('/tasks/:id', async function(req, res, next){
 router.post("/tasks",jsonParser, async function(req, res){
     // req.body.event.e_archived = false;
     console.log(req.body);
-    console.log("Attempting to create finding ");
+    console.log("Attempting to insert a task ");
     // req.body.event.e_assessment_date = new Date();
     // req.body.event.e_declassification_date = new Date();
     // req.body.analyst.a_initials = "EM"
